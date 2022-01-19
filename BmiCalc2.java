e6c07b7ba0528e2fafe5edda5a2823a6b8804c1a
r-hatako7814 <ryuichiro.hata@gmail.com>
2022-01-19 17:37:37 +0900

    47countUP

---
+++ BmiCalc2.java
@@ -0,0 +1,31 @@
+package jp.learningdesign.javastudy;
+
+import java.util.Scanner;
+
+public class BmiCalc2 {
+/*BMI(体格しい数を計算するプログラム)
+ * BNI = 体重　/ (身長 * 体重)
+ * 体重 kg、身長 cm
+ */
+	public static void main(String[] args) {
+		double weight,height,bmi;
+		
+		//height = 169;
+		//weight = 58;
+		
+		Scanner stdIn = new Scanner(System.in);
+		System.out.println("体重は？(kg) :");
+		weight = stdIn.nextDouble();
+		System.out.println("身長は？()cm:");
+		height = stdIn.nextDouble();
+		
+		
+		height /= 100;
+		
+		bmi = weight / (height * height);
+		
+		System.out.println("BMI:" + bmi + "です。");
+
+	}
+
+}
