99755600aa2ffbe899e7e75d627e76487fcfca38
r-hatako7814 <ryuichiro.hata@gmail.com>
2022-01-20 21:57:19 +0900

    52論理和完

---
+++ CommonMultiple2.java
@@ -0,0 +1,20 @@
+package jp.learningdesign.javastudy;
+
+public class CommonMultiple2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// 2または、３の倍数を、１から５０未満の範囲で表示するプログラム
+		int i = 1;
+		while (i < 50) {
+		    if ((i % 2 == 0) || (i % 3 == 0)) {
+		    	// 論理和
+		    	System.out.println(i);
+		    }
+		    i++; //インクリメント演算子
+		}
+		
+
+	}
+
+}
