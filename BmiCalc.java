0c7ed7732322b847c886c965e4e537654c265620
r-hatako7814 <ryuichiro.hata@gmail.com>
2022-01-20 21:22:33 +0900

    49BMI(importScanner,stdIn等を使ったBMI計算機)

--- BmiCalc.java
+++ BmiCalc.java
@@ -1,5 +1,7 @@
 package jp.learningdesign.javastudy;
 
+import java.util.Scanner;
+
 public class BmiCalc {
 /*BMI(体格しい数を計算するプログラム)
  * BNI = 体重　/ (身長 * 体重)
@@ -8,13 +10,19 @@ public class BmiCalc {
 	public static void main(String[] args) {
 		double weight,height,bmi;
 		
-		height = 180;
-		weight = 71;
+		//height = 180;
+		//weight = 71;
+		Scanner stdIn = new Scanner(System.in);
+		System.out.println("体重は？（kg）:");
+		weight = stdIn.nextDouble();
+		System.out.println("身長は？(cm):");
+		height = stdIn.nextDouble();
+		
 		height /= 100;
 		
 		bmi = weight / (height * height);
 		
-		System.out.println(bmi);
+		System.out.println("あなたのBMIは、" + bmi + "です。");
 		
 		if (bmi < 18.5) {
 			System.out.println("痩せ型です。");
