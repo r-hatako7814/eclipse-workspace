734eb01397c7a02017c626e323b8a91249747e8c
r-hatako7814 <ryuichiro.hata@gmail.com>
2022-01-20 22:03:22 +0900

    53not演算子完

---
+++ CommonMultiple3.java
@@ -0,0 +1,20 @@
+package jp.learningdesign.javastudy;
+
+public class CommonMultiple3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// ２と３の公倍数以外の数を表示する
+		int i = 1;
+		while (i < 20) {
+		    if (!((i % 2 == 0) && (i % 3 == 0))) {
+		    	//NOT演算子の適用例
+		    	System.out.println(i);
+		    }
+		    i++; //インクリメント演算子
+		}
+		
+
+	}
+
+}
