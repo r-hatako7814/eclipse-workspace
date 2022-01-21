5a6d4b7e9aba52c10a52e84d1aa1c2edff50f67f
r-hatako7814 <ryuichiro.hata@gmail.com>
2022-01-21 13:17:07 +0900

    57コンストラクタ完

--- StaffInfo.java
+++ StaffInfo.java
@@ -8,19 +8,24 @@ class Staff{
 	public void sayhello() {
 		System.out.println("Hello " + this.name);
 	}
+
+	public Staff(String name, int staffid, String email) {
+		super();
+		this.name = name;
+		this.staffid = staffid;
+		this.email = email;
+	}
 }
 
 public class StaffInfo {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		Staff yamada = new Staff();
-		yamada.name = "Taro Yamada";
+		Staff yamada = new Staff("Taro Yamada",12345,"yamada@abc.co.jp");
+		//yamada.name = "Taro Yamada";
         
 		yamada.sayhello();
 		
-		yamada.email = "123456@gmail.com";
-		System.out.println(yamada.email);
 
 	}
 
